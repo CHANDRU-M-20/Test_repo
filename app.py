c93e1b2a0d6997f311352d313e71e2226a0bf182
CHANDRU-M-20 <csechandruccse@gmail.com>
2025-03-08 08:50:38 +0530

    CreatedFile

---
+++ app.py
@@ -0,0 +1,13 @@
+# Image Detection
+
+from typing import List,Optional
+
+class Image_detector:
+    def __init__(self,image_byte:Optional[str]=None):
+        self.image_byte = image_byte
+
+
+# git add <file_name>
+# git commit -m "msg"
+
+
