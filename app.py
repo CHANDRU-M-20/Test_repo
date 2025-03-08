814ad6c1fc4508f025d41e61d1a91015adab795c
CHANDRU-M-20 <csechandruccse@gmail.com>
2025-03-08 14:15:52 +0530

    files added

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ class Image_detector:
         self.image_byte = image_byte
 
 
-# git --global user.name = "" 
+# git config --global user.name = "" 
 
 # git init
 # git status
 # git log
 
-# git init -b main
+# git init -b main [Change the branch]
 # git add <file_name> -->Staging ---> commit
 # git add . (If you want to add all the files into staging)
 # git commit -m "msg" -->File commited to the local repo
@@ -22,3 +22,8 @@ class Image_detector:
 
 # git remote add origin <repo>
 #git tag <tag_name eg.v1.0>
+# git push origin v1.0
+
+# Create branch
+# git checkout -b <breanch_name>
+# git branch
